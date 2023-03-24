0e4001f451408a39b71b9268ce7fdd7c06208c21
sagun-maharjan <maharjansagun121@gmail.com>
2023-03-24 11:51:19 -0400

    made changes to URL

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast{
   constructor(){
     this.key = 'XvIqM10L2A4vLlKszIvhaSLLwRrptpDb';
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
   }
   async updateCity(city){
     const cityDets = await this.getCity(city);
