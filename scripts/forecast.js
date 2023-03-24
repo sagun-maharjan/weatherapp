c0f99c8b1a91ce7caafec9ef82fbb7aadd801725
sagun-maharjan <maharjansagun121@gmail.com>
2023-03-24 11:41:20 -0400

    this is the first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,46 @@
+class Forecast{
+  constructor(){
+    this.key = 'XvIqM10L2A4vLlKszIvhaSLLwRrptpDb';
+    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  }
+  async updateCity(city){
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+
+    return {
+      cityDets: cityDets,
+      weather: weather
+
+      // since the value and property are the same above then we can write it like as follow:
+      // cityDets,
+      // weather
+    };
+  }
+  async getCity(city){
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI+query);
+    const data = await response.json();
+
+    return data[0];
+  }
+  
+  async getWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI+query);
+    const data = await response.json();
+
+    return data[0];
+  }
+}
+
+
+
+// getCity('kathmandu')
+//   .then(data => {
+//     return getWeather(data.Key);
+//   }).then(data => {
+//     console.log(data);
+//   }).catch(err => console.log(err));
+
+//getWeather('241809');
